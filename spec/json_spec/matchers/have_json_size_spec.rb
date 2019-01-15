ad0f40c8f91056399b9404dc4630e79a1a26a3da
Adam Gaynor <adam.gaynor@venuenext.com>
2019-01-14 21:34:42 -0500

    Convert rebased specs to rspec 3 syntax with Transpec.

--- spec/json_spec/matchers/have_json_size_spec.rb
+++ spec/json_spec/matchers/have_json_size_spec.rb
@@ -2,49 +2,49 @@ require "spec_helper"
 
 describe JsonSpec::Matchers::HaveJsonSize do
   it "counts array entries" do
-    %([1,2,3]).should have_json_size(3)
+    expect(%([1,2,3])).to have_json_size(3)
   end
 
   it "counts null array entries" do
-    %([1,null,3]).should have_json_size(3)
+    expect(%([1,null,3])).to have_json_size(3)
   end
 
   it "counts hash key/value pairs" do
-    %({"one":1,"two":2,"three":3}).should have_json_size(3)
+    expect(%({"one":1,"two":2,"three":3})).to have_json_size(3)
   end
 
   it "counts null hash values" do
-    %({"one":1,"two":null,"three":3}).should have_json_size(3)
+    expect(%({"one":1,"two":null,"three":3})).to have_json_size(3)
   end
 
   it "matches at a path" do
-    %({"one":[1,2,3]}).should have_json_size(3).at_path("one")
+    expect(%({"one":[1,2,3]})).to have_json_size(3).at_path("one")
   end
 
   it "provides a failure message" do
     matcher = have_json_size(3)
     matcher.matches?(%([1,2]))
-    matcher.failure_message.should            eq "Expected JSON value size to be 3, got 2"
-    matcher.failure_message_for_should.should eq "Expected JSON value size to be 3, got 2" # RSpec 2 interface
+    expect(matcher.failure_message).to            eq "Expected JSON value size to be 3, got 2"
+    expect(matcher.failure_message_for_should).to eq "Expected JSON value size to be 3, got 2" # RSpec 2 interface
   end
 
   it "provides a failure message for negation" do
     matcher = have_json_size(3)
     matcher.matches?(%([1,2,3]))
-    matcher.failure_message_when_negated.should   eq "Expected JSON value size to not be 3, got 3"
-    matcher.failure_message_for_should_not.should eq "Expected JSON value size to not be 3, got 3" # RSpec 2 interface
+    expect(matcher.failure_message_when_negated).to   eq "Expected JSON value size to not be 3, got 3"
+    expect(matcher.failure_message_for_should_not).to eq "Expected JSON value size to not be 3, got 3" # RSpec 2 interface
   end
 
   it "provides a description message" do
     matcher = have_json_size(1)
     matcher.matches?(%({"id":1,"json":["spec"]}))
-    matcher.description.should eq %(have JSON size "1")
+    expect(matcher.description).to eq %(have JSON size "1")
   end
 
   it "provides a description message with path" do
     matcher = have_json_size(1).at_path("json")
     matcher.matches?(%({"id":1,"json":["spec"]}))
-    matcher.description.should eq %(have JSON size "1" at path "json")
+    expect(matcher.description).to eq %(have JSON size "1" at path "json")
   end
 
   it "provides an error when parsing nil" do
--- spec/json_spec/matchers/have_json_type_spec.rb
+++ spec/json_spec/matchers/have_json_type_spec.rb
@@ -3,90 +3,90 @@ require "spec_helper"
 describe JsonSpec::Matchers::HaveJsonType do
   it "matches hashes" do
     hash = %({})
-    hash.should have_json_type(Hash)
-    hash.should have_json_type(:object)
+    expect(hash).to have_json_type(Hash)
+    expect(hash).to have_json_type(:object)
   end
 
   it "matches arrays" do
-    %([]).should have_json_type(Array)
+    expect(%([])).to have_json_type(Array)
   end
 
   it "matches at a path" do
-    %({"root":[]}).should have_json_type(Array).at_path("root")
+    expect(%({"root":[]})).to have_json_type(Array).at_path("root")
   end
 
   it "matches strings" do
-    %(["json_spec"]).should have_json_type(String).at_path("0")
+    expect(%(["json_spec"])).to have_json_type(String).at_path("0")
   end
 
   it "matches a valid JSON value, yet invalid JSON document" do
-    %("json_spec").should have_json_type(String)
+    expect(%("json_spec")).to have_json_type(String)
   end
 
   it "matches empty strings" do
-    %("").should have_json_type(String)
+    expect(%("")).to have_json_type(String)
   end
 
   it "matches integers" do
-    %(10).should have_json_type(Integer)
+    expect(%(10)).to have_json_type(Integer)
   end
 
   it "matches floats" do
-    %(10.0).should have_json_type(Float)
-    %(1e+1).should have_json_type(Float)
+    expect(%(10.0)).to have_json_type(Float)
+    expect(%(1e+1)).to have_json_type(Float)
   end
 
   it "matches booleans" do
-    %(true).should have_json_type(:boolean)
-    %(false).should have_json_type(:boolean)
+    expect(%(true)).to have_json_type(:boolean)
+    expect(%(false)).to have_json_type(:boolean)
   end
 
   it "matches ancestor classes" do
-    %(10).should have_json_type(Numeric)
-    %(10.0).should have_json_type(Numeric)
+    expect(%(10)).to have_json_type(Numeric)
+    expect(%(10.0)).to have_json_type(Numeric)
   end
 
   it "provides a failure message" do
     matcher = have_json_type(Numeric)
     matcher.matches?(%("foo"))
-    matcher.failure_message.should            eq "Expected JSON value type to be Numeric, got String"
-    matcher.failure_message_for_should.should eq "Expected JSON value type to be Numeric, got String" # RSpec 2 interface
+    expect(matcher.failure_message).to            eq "Expected JSON value type to be Numeric, got String"
+    expect(matcher.failure_message_for_should).to eq "Expected JSON value type to be Numeric, got String" # RSpec 2 interface
   end
 
   it "provides a failure message for negation" do
     matcher = have_json_type(Numeric)
     matcher.matches?(%(10.0))
 
-    matcher.failure_message_when_negated.should   eq "Expected JSON value type to not be Numeric, got Float"
-    matcher.failure_message_for_should_not.should eq "Expected JSON value type to not be Numeric, got Float" # RSpec 2 interface
+    expect(matcher.failure_message_when_negated).to   eq "Expected JSON value type to not be Numeric, got Float"
+    expect(matcher.failure_message_for_should_not).to eq "Expected JSON value type to not be Numeric, got Float" # RSpec 2 interface
   end
 
   it "provides a description message" do
     matcher = have_json_type(String)
     matcher.matches?(%({"id":1,"json":"spec"}))
-    matcher.description.should eq %(have JSON type "String")
+    expect(matcher.description).to eq %(have JSON type "String")
   end
 
   it "provides a description message with path" do
     matcher = have_json_type(String).at_path("json")
     matcher.matches?(%({"id":1,"json":"spec"}))
-    matcher.description.should eq %(have JSON type "String" at path "json")
+    expect(matcher.description).to eq %(have JSON type "String" at path "json")
   end
 
   context "somewhat uselessly" do
     it "matches true" do
-      %(true).should have_json_type(TrueClass)
+      expect(%(true)).to have_json_type(TrueClass)
     end
 
     it "matches false" do
-      %(false).should have_json_type(FalseClass)
+      expect(%(false)).to have_json_type(FalseClass)
     end
 
     it "matches null" do
       null = %(null)
-      null.should have_json_type(NilClass)
-      null.should have_json_type(:nil)
-      null.should have_json_type(:null)
+      expect(null).to have_json_type(NilClass)
+      expect(null).to have_json_type(:nil)
+      expect(null).to have_json_type(:null)
     end
   end
 end
